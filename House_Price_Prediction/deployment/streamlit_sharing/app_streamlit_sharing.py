d762aea85e3b5b57fcdc939604e41d4f8d61d682
Güneş Akbulut <gunesakbulut09@gmail.com>
2023-10-17 16:46:31 +0300

    d

--- House_Price_Prediction/deployment/streamlit_sharing/app_streamlit_sharing.py
+++ House_Price_Prediction/deployment/streamlit_sharing/app_streamlit_sharing.py
@@ -23,7 +23,7 @@ st.markdown("<h1 style='text-align: left; font-size: 22px; color: #008000;'> All
 
 # streamlit sharing
 # xx = pd.read_csv("/app/projects_and_workings/House_Price_Prediction/deployment/streamlit_sharing/df_new_grouped.csv")
-xx = pd.read_csv("/app/gakbulut/projects_and_workings/House_Price_Prediction/deployment/streamlit_sharing/df_new_grouped.csv")
+xx = pd.read_csv("/app/projects_and_workings/House_Price_Prediction/deployment/streamlit_sharing/df_new_grouped.csv")
 
 df_new_grouped = pd.DataFrame(xx)
 # df_new_grouped.set_index("date", inplace=True)
